28fdd1e1e4d89d2cab7abdea341225c9081747f4
Kazemaru_ <135320255+AhmadHasta@users.noreply.github.com>
2026-07-30 17:42:05 +0700

    feat: add support for vertical and horizontal split view orientation in the network inspector window (#2)

--- lib/dio_network_inspector.dart
+++ lib/dio_network_inspector.dart
@@ -4,3 +4,4 @@ export 'src/dio_network_inspector.dart';
 export 'src/dio_network_interceptor.dart';
 export 'src/features/overlay/overlay_widget.dart';
 export 'src/models/network_request.dart';
+export 'src/models/split_orientation.dart';
